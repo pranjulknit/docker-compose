319f7852c753fa1d9e5fb2baeb33f554ef7b9bb1
Pranjul Chaurasiya <chaurasiyapranjul@gmail.com>
2025-02-26 20:01:43 +0530

    postgres container added

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,28 @@
+import { PrismaClient } from "@prisma/client";
 import express from "express";
 
 const app = express();
 
+    const prismaClient = new PrismaClient();
 
-app.get("/",(req,res)=>{
+app.get("/",async (req,res)=>{
+    const data = await prismaClient.user.findMany();
+    console.log(data)
+
     res.json({
-        message:"get"
+        data
     })
 })
 
-app.post("/",(req,res)=>{
+app.post("/",async(req,res)=>{
+    await prismaClient.user.create({
+        data:{
+            username:Math.random().toString(),
+            password:Math.random().toString()
+        }
+      
+    });
+
     res.json({
         message:"post"
     })
